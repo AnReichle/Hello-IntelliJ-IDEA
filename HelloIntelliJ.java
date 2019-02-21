60ae140412462b2d7726b071e300185631d7ab17
Andreas <Andreas_Reichle_privat@web.de>
2019-02-21 17:29:48 +0100

    Add a message

--- HelloIntelliJ.java
+++ HelloIntelliJ.java
@@ -2,11 +2,12 @@ public class HelloIntelliJ {
 
     public static void main(String[] args) {
         System.out.println("Hello, IntelliJ!");
-        System.out.println("Goodby, IntelliJ!");
+        System.out.println("Goodbye, IntelliJ!");
         System.out.println("I will not be back");
         System.out.println("So don't wait for me");
         System.out.println("I forgot my violine!");
         System.out.println("This happens to me all the time ...");
+        System.out.println("It's not even funny anymore ... ");
     }
 
 }
