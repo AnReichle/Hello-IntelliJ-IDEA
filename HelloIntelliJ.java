9e8b6c88f1ee2b77fcbb34b980f694bd67c056ef
AnReichle <45435558+AnReichle@users.noreply.github.com>
2019-02-21 14:41:08 +0100

    Update HelloIntelliJ.java

--- HelloIntelliJ.java
+++ HelloIntelliJ.java
@@ -1,7 +1,8 @@
 public class HelloIntelliJ {
 
     public static void main(String[] args) {
-        System.out.println("Hello, IntelliJ");
+        System.out.println("Hello, IntelliJ!");
+        System.out.println("Goodby, IntelliJ!");
     }
 
 }
