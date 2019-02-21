becc5f4f325c5bac2517efd08c84ed6938525507
Andreas <Andreas_Reichle_privat@web.de>
2019-02-21 17:18:20 +0100

    Add a message

--- HelloIntelliJ.java
+++ HelloIntelliJ.java
@@ -6,7 +6,7 @@ public class HelloIntelliJ {
         System.out.println("I will not be back");
         System.out.println("So don't wait for me");
         System.out.println("I forgot my violine!");
-     
+        System.out.println("This happens to me all the time ...");
     }
 
 }
