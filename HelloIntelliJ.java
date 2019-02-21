c5769ba580aa7139d8b66f5013a2b332449d6a93
Andreas <Andreas_Reichle_privat@web.de>
2019-02-21 17:37:31 +0100

    Add a message

--- HelloIntelliJ.java
+++ HelloIntelliJ.java
@@ -8,6 +8,10 @@ public class HelloIntelliJ {
         System.out.println("I forgot my violine!");
         System.out.println("This happens to me all the time ...");
         System.out.println("It's not even funny anymore ... ");
+        System.out.println("The worst thing is: it's not even a violine. It's a bass!");
+        System.out.println("I always mix those two up ...");
+        System.out.println("And I am a professional musician!!!");
+
     }
 
 }
