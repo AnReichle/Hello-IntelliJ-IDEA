688d1624bb37645077de1e13a8bdabda51b74e4c
Andreas <Andreas_Reichle_privat@web.de>
2019-02-22 17:18:58 +0100

    Add gibberish

--- HelloIntelliJ.java
+++ HelloIntelliJ.java
@@ -1,6 +1,8 @@
 public class HelloIntelliJ {
 
     public static void main(String[] args) {
+        System.out.println("dfhjfdfbfh hfhf fhfj jfj jfjffjfj jfjfjfjfjfjfjfjf");
+
         System.out.println("Hello, IntelliJ!");
         System.out.println("Goodbye, IntelliJ!");
         System.out.println("I will not be back");
